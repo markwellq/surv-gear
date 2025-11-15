0b068d8f2920b8eb66b73f15c7589ac43ed29443
Denis Beccev <edisondts1@gmail.com>
2025-11-15 15:48:43 +0200

    Clickable products, category, show-product [without design, testing]

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,7 +10,7 @@ class ProductController extends Controller
     public function show($slug){
         $product = Product::where('slug', $slug)->firstOrFail();
 
-        return view('product_show',[
+        return view('product.show',[
             'product' => $product
         ]);
     }

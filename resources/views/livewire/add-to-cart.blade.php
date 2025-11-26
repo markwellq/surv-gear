d35169f75a3ffaf7c34ec52ab2b9f31584797dbc
Denis Beccev <edisondts1@gmail.com>
2025-11-26 21:22:28 +0200

    Final, read Readme, added link to google disk

--- resources/views/livewire/add-to-cart.blade.php
+++ resources/views/livewire/add-to-cart.blade.php
@@ -1,4 +1,4 @@
-<button wire:click="addToCart" wire:loading.attr="disabled" class="cursor-pointer py-2 border-[0.2px] px-6 border-white hover:bg-white/10">
+<button wire:click="addToCart" wire:loading.attr="disabled" class="cursor-pointer py-2 border-[0.1px] px-6 border-white hover:bg-white/10">
     <span class="text-white" wire:loading.remove>Add to backpack</span>
     <span class="text-white" wire:loading>Adding..</span>
 </button>
